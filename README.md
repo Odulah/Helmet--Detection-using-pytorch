0c23ac8650627976af6ef8e26b3f90aadda079e9
Odulah <aadim0311@gmail.com>
2023-10-16 00:47:18 +0530

    project done

---
+++ README.md
@@ -0,0 +1,5 @@
+# Helmet-Detection-PyTorch
+
+### Donwload data from here:
+
+https://drive.google.com/file/d/1Ae0qXg6rLOz9DUL-1DOAQfbQaw7oVryL/view?usp=drive_link
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="helmet",
+    version="0.0.1",
+    author="Aadil",
+    author_email="aadim0311@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
